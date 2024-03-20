4b3d4a1c43b06ad5ce2c8a3ec32c1b481b03e2c2
Aakash <aakashkumar2908@gmail.com>
2024-03-20 16:32:13 +0530

    seventh commit

--- models/order.js
+++ models/order.js
@@ -1,37 +1,3 @@
-// const mongoose = require('mongoose');
-
-// const orderSchema = new mongoose.Schema({
-//   userId: {
-//     type: String,
-//     required: true
-//   },
-//   productId: {
-//     type: String,
-//     required: true
-//   },
-//   size: {
-//     type: String,
-//     required: true
-//   },
-//   quantity: {
-//     type: Number,
-//     required: true
-//   },
-//   totalPrice: {
-//     type: Number,
-//     required: true
-//   },
-//   created_at: {
-//     type: Date,
-//     default: Date.now
-//   }
-// });
-
-// const Order = mongoose.model('Order', orderSchema);
-
-// module.exports = Order;
-
-
 const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,35 +1,7 @@
-// const express = require('express');
-// const router = express.Router();
-// const Order = require('../models/order');
-
-// router.post('/', async (req, res) => {
-//   try {
-//     const { userId, productId, size, quantity, totalPrice } = req.body;
-    
-//     const order = new Order({
-//       userId,
-//       productId,
-//       size,
-//       quantity,
-//       totalPrice
-//     });
-
-//     await order.save();
-//     res.status(201).json({ message: 'Order saved successfully' });
-//   } catch (error) {
-//     console.error('Error saving order:', error);
-//     res.status(500).json({ message: 'Internal server error' });
-//   }
-// });
-
-// module.exports = router;
-
-
 const express = require('express');
 const router = express.Router();
 const Order = require('../models/order');
 
-// POST request to create a new order
 router.post('/', async (req, res) => {
   try {
     const { userId, productId, size, quantity, totalPrice, name, address } = req.body;
@@ -52,15 +24,14 @@ router.post('/', async (req, res) => {
   }
 });
 
-// PUT request to update an existing order
 router.put('/', async (req, res) => {
   try {
     const { userId, productId } = req.body;
     
     const updatedOrder = await Order.findOneAndUpdate(
-      { userId, productId }, // Query criteria to find the order
-      req.body, // Updated order details
-      { new: true } // Option to return the updated document
+      { userId, productId },
+      req.body, 
+      { new: true }
     );
 
     if (!updatedOrder) {
@@ -73,5 +44,20 @@ router.put('/', async (req, res) => {
     res.status(500).json({ message: 'Internal server error' });
   }
 });
+
+router.get('/', async(req,res)=>{
+  res.json("hiii")
+})
+
+router.get('/:userId', async (req, res) => {
+  try {
+    const userId = req.params.userId;
+    const orders = await Order.find({ userId: userId });
+    res.json(orders);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 
 module.exports = router;
